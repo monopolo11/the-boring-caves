01ec97e6e81c727a34426ab8635c985e6041fac0
Bernardo Ruiz <bernardoruiz@bernardos-macbook-pro.local>
2019-03-25 18:34:37 -0600

    Initial Commit

---
+++ Proyecto final/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Proyecto final
+//
+//  Created by Bernardo Ruiz on 25/3/19.
+//  Copyright © 2019 Bernardo Ruiz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

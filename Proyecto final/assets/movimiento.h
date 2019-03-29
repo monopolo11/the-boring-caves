689695156bb00ff64797aac3da229be13f83a90e
Igo <is719485@iteso.mx>
2019-03-28 20:22:04 -0600

    Prueba visibilidad.

--- Proyecto final/assets/movimiento.h
+++ Proyecto final/assets/movimiento.h
@@ -2,18 +2,42 @@
 #define movimiento_h
 int llave = 0;
 
+void makevis(){
+  matrizvis[actrow-1][actcol-1]=1;
+  matrizvis[actrow-1][actcol]=1;
+  matrizvis[actrow-1][actcol+1]=1;
+  matrizvis[actrow][actcol-1]=1;
+  matrizvis[actrow][actcol]=1;
+  matrizvis[actrow][actcol+1]=1;
+  matrizvis[actrow+1][actcol-1]=1;
+  matrizvis[actrow+1][actcol]=1;
+  matrizvis[actrow+1][actcol+1]=1;
+
+  matrizvis[actrow-2][actcol-2]=1;
+  matrizvis[actrow-2][actcol]=1;
+  matrizvis[actrow-2][actcol+2]=1;
+  matrizvis[actrow][actcol-2]=1;
+  matrizvis[actrow][actcol]=1;
+  matrizvis[actrow][actcol+2]=1;
+  matrizvis[actrow+2][actcol-2]=1;
+  matrizvis[actrow+2][actcol]=1;
+  matrizvis[actrow+2][actcol+2]=1;
+}
+
 void moverArr(){
   int casilla = matriz[actrow-1][actcol];
   switch (casilla) {
     case 0:
       matriz[actrow][actcol]=0;
       matriz[actrow-1][actcol]=2;
+      makevis();
       actrow=actrow-1;
       printmap();
       break;
     case 5:
     matriz[actrow][actcol]=0;
     matriz[actrow-1][actcol]=2;
+    makevis();
     actrow=actrow-1;
     llave = 1;
     printmap();
@@ -30,12 +54,14 @@ void moverAba(){
     case 0:
       matriz[actrow][actcol]=0;
       matriz[actrow+1][actcol]=2;
+      makevis();
       actrow=actrow+1;
       printmap();
       break;
     case 5:
       matriz[actrow][actcol]=0;
       matriz[actrow+1][actcol]=2;
+      makevis();
       actrow=actrow+1;
       llave = 1;
       printmap();
@@ -60,12 +86,14 @@ void moverDer(){
     case 0:
       matriz[actrow][actcol]=0;
       matriz[actrow][actcol+1]=2;
+      makevis();
       actcol=actcol+1;
       printmap();
       break;
       case 5:
         matriz[actrow][actcol]=0;
         matriz[actrow][actcol+1]=2;
+        makevis();
         actcol=actcol+1;
         llave = 1;
         printmap();
@@ -90,12 +118,14 @@ void moverIzq(){
     case 0:
       matriz[actrow][actcol]=0;
       matriz[actrow][actcol-1]=2;
+      makevis();
       actcol=actcol-1;
       printmap();
       break;
       case 5:
         matriz[actrow][actcol]=0;
         matriz[actrow][actcol-1]=2;
+        makevis();
         actcol=actcol-1;
         llave = 1;
         printmap();
